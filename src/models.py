72a7fa0cf531cab33f9b5e96c192864ecdeab098
Sowjanya M <47984054+Janya21@users.noreply.github.com>
2025-04-15 15:49:51 +0530

    Update models.py with changes in Generator class

--- src/models.py
+++ src/models.py
@@ -18,12 +18,8 @@
 
 import torch
 import torch.nn as nn
-import torch.nn.init as init
 import torch.nn.functional as F
-
 from torch.autograd import Variable
-
-
 
 class MNIST_target_net(nn.Module):
     def __init__(self):
@@ -32,7 +28,6 @@ class MNIST_target_net(nn.Module):
         self.conv2 = nn.Conv2d(32, 32, kernel_size=3)
         self.conv3 = nn.Conv2d(32, 64, kernel_size=3)
         self.conv4 = nn.Conv2d(64, 64, kernel_size=3)
-
         self.fc1 = nn.Linear(64*4*4, 200)
         self.fc2 = nn.Linear(200, 200)
         self.logits = nn.Linear(200, 10)
@@ -51,26 +46,20 @@ class MNIST_target_net(nn.Module):
         x = self.logits(x)
         return x
 
-
 class Discriminator(nn.Module):
     def __init__(self, image_nc):
         super(Discriminator, self).__init__()
-        # MNIST: 1*28*28
         model = [
             nn.Conv2d(image_nc, 8, kernel_size=4, stride=2, padding=0, bias=True),
             nn.LeakyReLU(0.2),
-            # 8*13*13
             nn.Conv2d(8, 16, kernel_size=4, stride=2, padding=0, bias=True),
             nn.BatchNorm2d(16),
             nn.LeakyReLU(0.2),
-            # 16*5*5
             nn.Conv2d(16, 32, kernel_size=4, stride=2, padding=0, bias=True),
             nn.BatchNorm2d(32),
             nn.LeakyReLU(0.2),
             nn.Conv2d(32, 1, 1),
-            # 32*1*1
         ]
-        
         self.model = nn.Sequential(*model)
         self.sigmoid = nn.Sigmoid()
 
@@ -79,59 +68,62 @@ class Discriminator(nn.Module):
         output = self.sigmoid(logits).squeeze()
         return logits, output
 
-
 class Generator(nn.Module):
     def __init__(self, gen_input_nc, image_nc, target='Auto'):
         super(Generator, self).__init__()
 
+        # Dynamic filtering layer to predict noise-suppressing kernel
+        self.filter_estimator = nn.Sequential(
+            nn.Conv2d(gen_input_nc, 32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 16, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(16, 9, kernel_size=3, stride=1, padding=1),  # 3x3 kernel
+            nn.Tanh()  # Normalize kernel values
+        )
+
+        # Encoder
         encoder_lis = [
-            # MNIST:1*28*28
             nn.Conv2d(gen_input_nc, 8, kernel_size=3, stride=1, padding=0, bias=True),
             nn.InstanceNorm2d(8),
             nn.ReLU(),
-            # 8*26*26
             nn.Conv2d(8, 16, kernel_size=3, stride=2, padding=0, bias=True),
             nn.InstanceNorm2d(16),
             nn.ReLU(),
-            # 16*12*12
             nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=0, bias=True),
             nn.InstanceNorm2d(32),
             nn.ReLU(),
-            # 32*5*5
         ]
 
-        bottle_neck_lis = [ResnetBlock(32),
-                       ResnetBlock(32),
-                       ResnetBlock(32),
-                       ResnetBlock(32),]
+        # Bottleneck
+        bottle_neck_lis = [
+            ResnetBlock(32),
+            ResnetBlock(32),
+            ResnetBlock(32),
+            ResnetBlock(32),
+        ]
 
+        # Decoder
         if target == 'HighResolution':
             decoder_lis = [
                 nn.ConvTranspose2d(32, 16, kernel_size=3, stride=2, padding=0, bias=False),
                 nn.InstanceNorm2d(16),
                 nn.ReLU(),
-                # state size. 16 x 11 x 11
                 nn.ConvTranspose2d(16, 8, kernel_size=3, stride=2, padding=0, bias=False),
                 nn.InstanceNorm2d(8),
                 nn.ReLU(),
-                # state size. 8 x 23 x 23
                 nn.ConvTranspose2d(8, image_nc, kernel_size=5, stride=1, padding=0, bias=False),
-                # nn.Tanh()
-                # state size. image_nc x 28 x 28
             ]
         else:
             decoder_lis = [
                 nn.ConvTranspose2d(32, 16, kernel_size=3, stride=2, padding=0, bias=False),
                 nn.InstanceNorm2d(16),
                 nn.ReLU(),
-                # state size. 16 x 11 x 11
                 nn.ConvTranspose2d(16, 8, kernel_size=3, stride=2, padding=0, bias=False),
                 nn.InstanceNorm2d(8),
                 nn.ReLU(),
-                # state size. 8 x 23 x 23
                 nn.ConvTranspose2d(8, image_nc, kernel_size=6, stride=1, padding=0, bias=False),
                 nn.Tanh()
-                # state size. image_nc x 28 x 28
             ]
 
         self.encoder = nn.Sequential(*encoder_lis)
@@ -139,10 +131,56 @@ class Generator(nn.Module):
         self.decoder = nn.Sequential(*decoder_lis)
 
     def forward(self, x):
-        x = self.encoder(x)
+        # Predict dynamic filter (3x3 kernel)
+        filter_kernel = self.filter_estimator(x)  # [batch, 9, h, w]
+        filter_kernel = filter_kernel.view(-1, 1, 3, 3)  # [batch*9, 1, 3, 3]
+        batch_size = x.size(0)
+        x_padded = F.pad(x, (1, 1, 1, 1), mode='reflect')  # Pad for 3x3 conv
+        filtered_x = F.conv2d(x_padded, filter_kernel, groups=batch_size)  # Apply dynamic filter
+
+        # GAN pipeline
+        x = self.encoder(filtered_x)
         x = self.bottle_neck(x)
         x = self.decoder(x)
         return x
+
+class ResnetBlock(nn.Module):
+    def __init__(self, dim, padding_type='reflect', norm_layer=nn.BatchNorm2d, use_dropout=False, use_bias=False):
+        super(ResnetBlock, self).__init__()
+        self.conv_block = self.build_conv_block(dim, padding_type, norm_layer, use_dropout, use_bias)
+
+    def build_conv_block(self, dim, padding_type, norm_layer, use_dropout, use_bias):
+        conv_block = []
+        p = 0
+        if padding_type == 'reflect':
+            conv_block += [nn.ReflectionPad2d(1)]
+        elif padding_type == 'replicate':
+            conv_block += [nn.ReplicationPad2d(1)]
+        elif padding_type == 'zero':
+            p = 1
+        else:
+            raise NotImplementedError('padding [%s] is not implemented' % padding_type)
+        conv_block += [nn.Conv2d(dim, dim, kernel_size=3, padding=p, bias=use_bias),
+                       norm_layer(dim),
+                       nn.ReLU(True)]
+        if use_dropout:
+            conv_block += [nn.Dropout(0.5)]
+        p = 0
+        if padding_type == 'reflect':
+            conv_block += [nn.ReflectionPad2d(1)]
+        elif padding_type == 'replicate':
+            conv_block += [nn.ReplicationPad2d(1)]
+        elif padding_type == 'zero':
+            p = 1
+        else:
+            raise NotImplementedError('padding [%s] is not implemented' % padding_type)
+        conv_block += [nn.Conv2d(dim, dim, kernel_size=3, padding=p, bias=use_bias),
+                       norm_layer(dim)]
+        return nn.Sequential(*conv_block)
+
+    def forward(self, x):
+        out = x + self.conv_block(x)
+        return out
 
 
 # Define a resnet block
